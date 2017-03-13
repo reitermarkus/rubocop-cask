80f94ed3123223662bb45aea2abfb6a9f9462727
Markus Reiter <me@reitermark.us>
2017-03-14 00:19:32 +0100

    Add HomepageUrlTrailingSlash cop.

--- lib/rubocop-cask.rb
+++ lib/rubocop-cask.rb
@@ -12,7 +12,9 @@ require 'rubocop/cask/inject'
 RuboCop::Cask::Inject.defaults!
 
 require 'rubocop/cop/cask/mixin/cask_help'
+require 'rubocop/cop/cask/mixin/on_homepage_stanza'
 require 'rubocop/cop/cask/homepage_matches_url'
+require 'rubocop/cop/cask/homepage_url_trailing_slash'
 require 'rubocop/cop/cask/no_dsl_version'
 require 'rubocop/cop/cask/stanza_order'
 require 'rubocop/cop/cask/stanza_grouping'
---
+++ spec/rubocop/cop/cask/homepage_url_trailing_slash_spec.rb
@@ -0,0 +1,53 @@
+describe RuboCop::Cop::Cask::HomepageUrlTrailingSlash do
+  include CopSharedExamples
+
+  subject(:cop) { described_class.new }
+
+  context 'when the homepage url ends with a slash' do
+    let(:source) do
+      <<-CASK.undent
+        cask 'foo' do
+          homepage 'https://foo.example.com/'
+        end
+      CASK
+    end
+
+    include_examples 'does not report any offenses'
+  end
+
+  context 'when the homepage url does not end with a slash' do
+    context 'but has a path' do
+      let(:source) do
+        <<-CASK.undent
+          cask 'foo' do
+            homepage 'https://foo.example.com/path'
+          end
+        CASK
+      end
+
+      include_examples 'does not report any offenses'
+    end
+
+    context 'and has no path' do
+      let(:source) do
+        <<-CASK.undent
+          cask 'foo' do
+            homepage 'https://foo.example.com'
+          end
+        CASK
+      end
+      let(:expected_offenses) do
+        [{
+          message: "'https://foo.example.com' must have a slash "\
+                   'after the domain.',
+          severity: :convention,
+          line: 2,
+          column: 11,
+          source: "'https://foo.example.com'"
+        }]
+      end
+
+      include_examples 'reports offenses'
+    end
+  end
+end
